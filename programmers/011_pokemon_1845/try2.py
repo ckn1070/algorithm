878c0ae3f13fbce89cd907c579ce7333e8abcd9d
CKNam <ckn1070@naver.com>
2026-09-14 10:40:00 +0900

    add: 011 폰켓몬 2차

---
+++ programmers/011_pokemon_1845/try2.py
@@ -0,0 +1,8 @@
+def solution(nums):
+    answer = min(len(set(nums)), len(nums)/2)
+    print('answer', answer)
+    return int(min(len(set(nums)), len(nums)/2))
+
+solution([3,1,2,3])
+solution([3,3,3,2,2,4])
+solution([3,3,3,2,2,2]	)
